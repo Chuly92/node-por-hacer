42be64273b43798afa14b612aca64702f562afb7
Julieta <julietagallego@live.com.ar>
2020-08-28 21:24:31 -0300

    Commit inicial

---
+++ config/yargs.js
@@ -0,0 +1,30 @@
+const descripcion = {
+    demand: true,
+    alias: 'd',
+    desc: 'Describe la tarea por hacer'
+};
+
+const completado = {
+    default: true,
+    alias: 'c',
+    desc: 'Marca como completada la tarea'
+};
+
+const argv = require('yargs')
+    .command('crear', 'Crear una tarea por hacer', {
+        descripcion
+    })
+    .command('actualizar', 'Actualiza una tarea por hacer', {
+        descripcion,
+        completado
+    })
+    .command('borrar', 'Elimina una tarea', {
+        descripcion
+    })
+    .help()
+    .argv;
+
+
+module.exports = {
+    argv
+}
